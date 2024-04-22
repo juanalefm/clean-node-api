92692b69f24e85ba7cedf1261f98b0e59bb10600
Juan <juanalef.m@gmail.com>
2024-04-21 22:28:43 -0300

    test: ensure SignUpController return 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -172,7 +172,7 @@ describe('SignUpController', () => {
   })
 })
 
-// Se o servidor der erro retorna serverError
+// Se der algum erro no servidor na parte do emailValidator retorna 500
 describe('SignUpController', () => {
   test('Should return 500 if EmailValidator throws', () => {
     const { sut, emailValidatorStub } = makeSut()
@@ -214,7 +214,7 @@ describe('SignUpController', () => {
   })
 })
 
-// Se o servidor der erro retorna serverError
+// Se der algum erro no servidor na parte do addAccount retorna 500
 describe('SignUpController', () => {
   test('Should return 500 if AddAccount throws', () => {
     const { sut, addAccountStub } = makeSut()
@@ -232,5 +232,27 @@ describe('SignUpController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+})
+
+describe('SignUpController', () => {
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
   })
 })
